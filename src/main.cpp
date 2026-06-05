b1ec20f676d6d49c11eaf6aa9d9fb56f627d9a1e
Eruza2k6 <erzadwiputra01@gmail.com>
2026-06-05 08:47:04 +0700

    Add Arduino code for water level monitoring system
    This code initializes an Arduino project to monitor water levels and control a pump and valve based on sensor readings.

---
+++ src/main.cpp
@@ -0,0 +1,92 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+// ===== PIN =====
+#define SENSOR_PIN 34
+#define SERVO_PIN 25
+#define POMPA_PIN 18
+#define VALVE_PIN 27
+#define FLOW_SENSOR 15
+
+Servo myServo;
+
+// ===== SENSOR DEBIT =====
+volatile int pulseCount = 0;
+float flowRate;
+
+// ===== SETTING =====
+int batasAir = 2000; // sesuaikan hasil kalibrasi sensor
+
+// ===== INTERRUPT SENSOR DEBIT =====
+void IRAM_ATTR countPulse() {
+  pulseCount++;
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(SENSOR_PIN, INPUT);
+  pinMode(POMPA_PIN, OUTPUT);
+  pinMode(VALVE_PIN, OUTPUT);
+
+  // sensor debit
+  pinMode(FLOW_SENSOR, INPUT_PULLUP);
+
+  // interrupt sensor debit
+  attachInterrupt(
+    digitalPinToInterrupt(FLOW_SENSOR),
+    countPulse,
+    FALLING
+  );
+
+  myServo.attach(SERVO_PIN);
+
+  // kondisi awal
+  digitalWrite(POMPA_PIN, LOW);
+  digitalWrite(VALVE_PIN, LOW);
+}
+
+void loop() {
+
+  // ===== SENSOR AIR =====
+  int nilaiAir = analogRead(SENSOR_PIN);
+
+  Serial.print("Level Air: ");
+  Serial.println(nilaiAir);
+
+  // ===== HITUNG DEBIT =====
+  pulseCount = 0;
+
+  delay(1000);
+
+  // rumus umum YF-S201
+  flowRate = pulseCount / 7.5;
+
+  Serial.print("Debit Air: ");
+  Serial.print(flowRate);
+  Serial.println(" L/min");
+
+  // ===== KONDISI AIR RENDAH =====
+  if (nilaiAir < batasAir) {
+
+    Serial.println("Air RENDAH");
+
+    myServo.write(45);               // buka pintu
+    digitalWrite(POMPA_PIN, HIGH);  // pompa ON
+    digitalWrite(VALVE_PIN, LOW);   // valve OFF
+  }
+
+  // ===== KONDISI AIR TINGGI =====
+  else {
+
+    Serial.println("Air TINGGI");
+
+    myServo.write(0);                // tutup pintu
+    digitalWrite(POMPA_PIN, LOW);   // pompa OFF
+    digitalWrite(VALVE_PIN, HIGH);  // valve ON
+  }
+
+  Serial.println("------------------");
+}
+
+//Notes: Everything is built using AI
